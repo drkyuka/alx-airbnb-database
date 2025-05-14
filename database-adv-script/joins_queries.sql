f1ae3a5be6b9de4260b47ed4ac3eec65f2ff33bc
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-05-14 11:03:54 +0100

    refactor SQL queries to simplify joins and improve readability

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,10 +1,6 @@
 -- query using an `INNER JOIN` to retrieve all bookings and the respective users who made those bookings.
 
 select Booking.booking_id,
-       Booking.start_date,
-       Booking.end_date,
-       Booking.status,
-       User.user_id,
        User.first_name,
        User.last_name 
     from `Booking`
@@ -12,13 +8,8 @@ select Booking.booking_id,
     on `Booking`.`user_id` = `User`.`user_id`;
 
 -- Query using a `LEFT JOIN` to retrieve all properties and their reviews, including properties that have no reviews
-select Property.property_id,
-       Property.name,
-       Property.location,
-       Property.pricepernight,
-       Review.review_id,
-       Review.rating,
-       Review.comment 
+select Property.name,
+       Review.comment
     from `Property`
     left JOIN `Review`
     on `Property`.`property_id` = `Review`.`property_id`;
@@ -26,22 +17,16 @@ select Property.property_id,
 
 -- Query using a `FULL OUTER JOIN` to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 -- mysql does not support FULL OUTER JOIN directly, but you can achieve it using a combination of LEFT JOIN and RIGHT JOIN with UNION
-select User.user_id,
-       User.first_name,
+select User.first_name,
        User.last_name,
-       Booking.booking_id,
-       Booking.start_date,
-       Booking.end_date 
+       Booking.booking_id
     from `User`
-    left JOIN `Booking`
+    left join `Booking`
     on `User`.`user_id` = `Booking`.`user_id`
-    UNION
-    select User.user_id,
-       User.first_name,
+    union
+    select User.first_name,
        User.last_name,
-       Booking.booking_id,
-       Booking.start_date,
-       Booking.end_date 
+       Booking.booking_id
     from `User`
-    right JOIN `Booking`
+    right join `Booking`
     on `User`.`user_id` = `Booking`.`user_id`;
