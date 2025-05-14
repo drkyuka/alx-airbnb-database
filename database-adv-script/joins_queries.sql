4acc34fd9a3e874bcf8b7e02b01675dc9c00793c
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-05-14 11:09:33 +0100

    add ORDER BY clause to LEFT JOIN query for consistent sorting of property names

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -12,7 +12,8 @@ SELECT Property.name,
        Review.comment
     FROM `Property`
     LEFT JOIN `Review`
-    ON `Property`.`property_id` = `Review`.`property_id`;
+    ON `Property`.`property_id` = `Review`.`property_id`, 
+    ORDER BY Property.name;
 
 
 -- Query using a `FULL OUTER JOIN` to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
