d21ef5195e7ccb660528f2c16d8f070b3c1af600
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-05-14 11:08:12 +0100

    capitalize SQL keywords for consistency and improved readability

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,32 +1,32 @@
 -- query using an `INNER JOIN` to retrieve all bookings and the respective users who made those bookings.
 
-select Booking.booking_id,
+SELECT Booking.booking_id,
        User.first_name,
        User.last_name 
-    from `Booking`
-    inner JOIN `User`
-    on `Booking`.`user_id` = `User`.`user_id`;
+    FROM `Booking`
+    INNER JOIN `User`
+    ON `Booking`.`user_id` = `User`.`user_id`;
 
 -- Query using a `LEFT JOIN` to retrieve all properties and their reviews, including properties that have no reviews
-select Property.name,
+SELECT Property.name,
        Review.comment
-    from `Property`
-    left JOIN `Review`
-    on `Property`.`property_id` = `Review`.`property_id`;
+    FROM `Property`
+    LEFT JOIN `Review`
+    ON `Property`.`property_id` = `Review`.`property_id`;
 
 
 -- Query using a `FULL OUTER JOIN` to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 -- mysql does not support FULL OUTER JOIN directly, but you can achieve it using a combination of LEFT JOIN and RIGHT JOIN with UNION
-select User.first_name,
+SELECT User.first_name,
        User.last_name,
        Booking.booking_id
-    from `User`
-    left join `Booking`
-    on `User`.`user_id` = `Booking`.`user_id`
+    FROM `User`
+    LEFT JOIN `Booking`
+    ON `User`.`user_id` = `Booking`.`user_id`
     union
-    select User.first_name,
+    SELECT User.first_name,
        User.last_name,
        Booking.booking_id
-    from `User`
-    right join `Booking`
-    on `User`.`user_id` = `Booking`.`user_id`;
+    FROM `User`
+    RIGHT JOIN `Booking`
+    ON `User`.`user_id` = `Booking`.`user_id`;
