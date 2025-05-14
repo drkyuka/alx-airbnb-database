c30ca6592b2fba9c098ba0f37d32d414ee1e253a
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-05-14 11:14:19 +0100

    capitalize SQL keywords and standardize alias naming for improved readability

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,21 +1,22 @@
 -- Query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.
-select User.user_id,
+SELECT User.user_id,
        User.first_name,
        User.last_name,
-       COUNT(Booking.booking_id) AS total_bookings
-from `User`
-inner join `Booking` 
-on User.user_id = Booking.user_id
-group by User.user_id;
+       COUNT(Booking.booking_id) AS NUMBER_OF_BOOKINGS
+FROM `User`
+INNER JOIN `Booking` 
+ON User.user_id = Booking.user_id
+GROUP BY User.user_id;
 
 -- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
-select 
+SELECT 
     P.property_id,
     P.name,
     P.location,
-    COUNT(B.booking_id) AS total_bookings,
+    COUNT(B.booking_id) AS NUMBER_OF_BOOKINGS,
     row_number() over (order by COUNT(B.booking_id) desc) as booking_row_number,
     rank() over (order by COUNT(B.booking_id) DESC) AS booking_rank
-from `Property` P
-inner join `Booking` B ON P.property_id = B.property_id
-group by P.property_id, P.name, P.location;
+FROM `Property` P
+INNER JOIN  `Booking` B 
+ON P.property_id = B.property_id
+GROUP BY P.property_id, P.name, P.location;
